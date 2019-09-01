3bacf619c189fc67c5ce6b342be7dcf4e186bec6
Gergely Kocsis <koger23@gmail.com>
2019-09-01 08:46:28 +0200

    feat(App): add weather and form fill with getWeather method

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Form extends Component {
+class Form extends React.Component {
     render() {
         return (
             <div>
-                Form Component
+                <form onSubmit={this.props.getWeather}>
+                    <input type="text" name="city" placeholder="City..."/>
+                    <input type="text" name="country" placeholder="Country..."/>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
 }
 
-export default Form
+export default Form;
