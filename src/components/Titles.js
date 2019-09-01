63deab4310c70d215791a92086c7c0b089eb50f5
Gergely Kocsis <koger23@gmail.com>
2019-09-01 09:19:23 +0200

    feat(*): format components with css

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 const Titles = props => (
     <div>
-        <h1>Wheather Finder</h1>
-        <p>Find out temperature, condition and more...</p>
+        <h1 className="title-container__title">Wheather Finder</h1>
+        <h3 className="title-container__subtitle">Find out temperature, condition and more...</h3>
     </div>);
 
 export default Titles
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 const Weather = props => (
-    <div>
-        {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-        {props.temperature && <p>Temperature: {props.temperature}</p>}
-        {props.humidty && <p>Humidity: {props.humidity}</p>}
-        {props.description && <p> Conditions: {props.description}</p>}
-        {props.error && <p>{props.error}</p>}
+    <div className="weather__info">
+        {
+            props.city && props.country && <p className="weather__key">Location: 
+            <span className="weather__value"> {props.city}, {props.country}</span>
+            </p>
+        }
+        {
+            props.temperature && <p className="weather__key">Temperature: 
+            <span className="weather__value"> {props.temperature}</span>
+            </p>
+        }
+        {
+            props.humidty && <p className="weather__key">Humidity: 
+            <span className="weather__value"> props.humidity}</span>
+            </p>
+        }
+        {
+            props.description && <p className="weather__key"> Conditions: 
+            <span className="weather__value"> {props.description}</span>
+            </p>
+        }
+        {
+            props.error && <p className="weather__error">{props.error}</p>
+        }
     </div>
 );
 
